16e7e3eaab17518880af7e87ea515172207e6798
ohjisu320 <ohjisu320@gmail.com>
2024-01-25 10:14:27 +0900

    commit

--- docs/quests/browser_js/js/datagokr_ad_count_by_times.js
+++ docs/quests/browser_js/js/datagokr_ad_count_by_times.js
@@ -21,33 +21,34 @@ window.onload = async (event) => {
         previous_click.addEventListener('click', (event) => {
             let ad_array = ""
             console.log(event.code)
-            try {
-                start_num = start_num-10
-                end_num =end_num-10
-                for (let ad_obj of ad_all.slice(start_num-10,end_num-10)){
-                    ad_array = `${ad_array}<tr><td>${ad_obj["BRDC_YMD"]}</td><td>${ad_obj["BRDC_WKD"]}</td><td>${ad_obj["BRDC_TM_RNG"]}</td><td>${ad_obj["TPBIZ_NM_1"]}</td><td>${ad_obj["TPBIZ_NM_2"]}</td><td>${ad_obj["TPBIZ_NM_3"]}</td><td>${ad_obj["AD_CNT"]}</td></tr>`
-                    };
-                ad_count_element.innerHTML = ad_array
-            } catch (error) {
+            start_num = start_num-10
+            end_num =end_num-10
+
+            for (let ad_obj of ad_all.slice(start_num,end_num)){
+                ad_array = `${ad_array}<tr><td>${ad_obj["BRDC_YMD"]}</td><td>${ad_obj["BRDC_WKD"]}</td><td>${ad_obj["BRDC_TM_RNG"]}</td><td>${ad_obj["TPBIZ_NM_1"]}</td><td>${ad_obj["TPBIZ_NM_2"]}</td><td>${ad_obj["TPBIZ_NM_3"]}</td><td>${ad_obj["AD_CNT"]}</td></tr>`
+                };
+            ad_count_element.innerHTML = ad_array
+            if (start_num<0) {
                 let message = "이전 데이터가 없습니다."
-                ad_count_element.innerHTML = message     
+                ad_count_element.innerHTML = message   
             }
+
         });
         next_click.addEventListener('click', (event)=>{
             let ad_array = ""
-            try {
-                start_num = start_num+10
-                end_num =end_num+10
-                for (let ad_obj of ad_all.slice(start_num,end_num)){
-                    ad_array = `${ad_array}<tr><td>${ad_obj["BRDC_YMD"]}</td><td>${ad_obj["BRDC_WKD"]}</td><td>${ad_obj["BRDC_TM_RNG"]}</td><td>${ad_obj["TPBIZ_NM_1"]}</td><td>${ad_obj["TPBIZ_NM_2"]}</td><td>${ad_obj["TPBIZ_NM_3"]}</td><td>${ad_obj["AD_CNT"]}</td></tr>`
-                    };
-                ad_count_element.innerHTML = ad_array
-            } catch (error) {
+           
+            start_num = start_num+10
+            end_num =end_num+10
+            for (let ad_obj of ad_all.slice(start_num,end_num)){
+                ad_array = `${ad_array}<tr><td>${ad_obj["BRDC_YMD"]}</td><td>${ad_obj["BRDC_WKD"]}</td><td>${ad_obj["BRDC_TM_RNG"]}</td><td>${ad_obj["TPBIZ_NM_1"]}</td><td>${ad_obj["TPBIZ_NM_2"]}</td><td>${ad_obj["TPBIZ_NM_3"]}</td><td>${ad_obj["AD_CNT"]}</td></tr>`
+                };
+            ad_count_element.innerHTML = ad_array
+            if (start_num>=50) {
                 let message = "다음 데이터가 없습니다."
-                ad_count_element.innerHTML = message}
-            });
-    } catch (error) {
-        console.log(`${error.message}`)
+                ad_count_element.innerHTML = message   
+            }})
+        } catch (error) {
+            console.log(`${error.message}`)
     };
 }
 
